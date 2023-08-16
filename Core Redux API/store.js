4f2f08f8145b6909beccbaa71fcae06b4bfc0841
felipelv97 <felipelv97@gmail.com>
2023-08-16 17:34:18 -0500

    store

--- Core Redux API/store.js
+++ Core Redux API/store.js
@@ -1,1 +1,29 @@
 import { createStore } from "redux";
+
+const initialState = 0;
+const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(countReducer);
+
+store.dispatch({ type: "increment" });
+console.log(store.getState());
+store.dispatch({ type: "increment" });
+console.log(store.getState());
+
+store.dispatch({ type: "decrement" });
+console.log(store.getState());
+
+store.dispatch({ type: "decrement" });
+console.log(store.getState());
+
+store.dispatch({ type: "decrement" });
+console.log(store.getState());
