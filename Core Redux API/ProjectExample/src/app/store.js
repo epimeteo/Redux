df1d721d5edd373ea05dcd969ef69934f67c254d
felipelv97 <felipelv97@gmail.com>
2023-08-18 14:39:23 -0500

    project example recipes

---
+++ Core Redux API/ProjectExample/src/app/store.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers } from "redux";
+
+import { allRecipesReducer } from "../features/allRecipes/allRecipesSlice.js";
+import { favoriteRecipesReducer } from "../features/favoriteRecipes/favoriteRecipesSlice.js";
+import { searchTermReducer } from "../features/searchTerm/searchTermSlice.js";
+
+const reducers = {
+  allRecipes: allRecipesReducer,
+  favoriteRecipes: favoriteRecipesReducer,
+  searchTerm: searchTermReducer,
+};
+
+export const store = createStore(combineReducers(reducers));
